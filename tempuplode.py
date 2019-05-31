44a7cae40f7d6c9843d294fffa74ffb202690f08
Tong Tiantai <wyexttt@163.com>
2019-05-31 13:15:18 +0800

    Add files via upload

--- tempuplode.py
+++ tempuplode.py
@@ -1,109 +1,107 @@
 #!/usr/bin/env python
-import temp
-import tempuplode
-import RPi.GPIO as GPIO
-import time
-import schedule
-import os
-import threading
-#declear of relay
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 from firebase import firebase
-#firebase = firebase.FirebaseApplication('https://testproject-9c322.firebaseio.com/')
+import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore
+import time
+from datetime import datetime, timedelta
+import temp
+import sys
+import os
+
+new = []
+new1 = []
+
+#cred = credentials.Certificate("./serviceKey.json")
+#app = firebase_admin.initialize_app(cred)
+#store = firestore.client()
+
+
 newdb = firebase.FirebaseApplication('https://aquaculture-7393d.firebaseio.com/')
+#firebase = firebase.FirebaseApplication('https://testproject-9c322.firebaseio.com/')
 
-Relay_Ch1 = 26
-Relay_Ch2 = 20
-Relay_Ch3 = 21
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(Relay_Ch1,GPIO.OUT)
-GPIO.setup(Relay_Ch2,GPIO.OUT)
-GPIO.setup(Relay_Ch3,GPIO.OUT)
-
-GPIO.output(Relay_Ch1,GPIO.HIGH)
-GPIO.output(Relay_Ch2,GPIO.HIGH)
-GPIO.output(Relay_Ch3,GPIO.HIGH)
-#relay setup defult off
-
-high=newdb.get('pi1-pond1', "high")
-low=newdb.get('pi1-pond1', "low")
-
-b=300
-'''
-def ch2On():
-    GPIO.output(Relay_Ch2,GPIO.LOW)
-    print("Channel 2: ON")
-    newdb.put('pi1-pond1',"ch2",1)
-    time.sleep(60);
-    GPIO.output(Relay_Ch2,GPIO.HIGH)
-    print("Channel 2: OFF")
-    newdb.put('pi1-pond1',"ch2",0)
-
-def ch2Off():
-    GPIO.output(Relay_Ch2,GPIO.HIGH)
-    print("Channel 2: OFF")
-    newdb.put('pi1-pond1',"ch2",0)
-'''
-
-def run_threaded(job_func):
-     job_thread = threading.Thread(target=job_func)
-     job_thread.start()
-    
-
-schedule.every().hour.at(":00").do(run_threaded, tempuplode.up)
-schedule.every(10).minutes.do(run_threaded, tempuplode.upa)
-#schedule.every(1).minutes.do(run_threaded, ch2On)
-#schedule.every(2).minutes.do(run_threaded, ch2Off)
-
-while True:
-    a=temp.getTemp()
-    tempuplode.rec()
-    exit_code = os.system('ping -c 1 www.google.com')
-    if exit_code:
-        print("No Internet Connection, Offline Model")
-        if a > high or a < low:
-            GPIO.output(Relay_Ch1,GPIO.LOW)
-            print("Channel 1:ON")
-        else:
-            GPIO.output(Relay_Ch1,GPIO.HIGH)
-            print("Channel 1:OFF")
+with open('save.txt', 'r+') as f:
+    line=f.readline()
+    if line != "":
+        line=line.replace("\n", "")
+        line=line.strip()
+        line=line.replace(' ', '')
+        c=line.replace("\r", "")
+        c=int(c)
+        new.append(c)
+        
+with open('save1.txt', 'r+') as f2:
+    line1=f2.readline()
+    if line1 != "":
+        line1=line1.replace("\n", "")
+        line1=line1.strip()
+        line1=line1.replace(' ', '')
+        d=line1.replace("\r", "")
+        d=int(d)
+        new1.append(d)
+        
+        
+"""
+def calTemp():
+    if len(new) > 50:
+        nsum=0
+        nsum+=sum(new)
+        average=nsum/len(new)
+        #data={u'val' :average}
+        #doc_ref = store.collection(u'pi/pi1/pond1/temp')
+        #doc_ref.add({u'val': average, u'time': (datetime.now()-timedelta(hours=8))})
+        #data={u(datetime.now()-timedelta(hours=8)): average}
+        #store.collection(u'pi/pi1/pond1').document(u'temp').set(data)
+        #firebase.put('pi1-temp', datetime.now()-timedelta(hours=8), average)
+        #newdb.put("/pi1-temp", datetime.now().strftime("%b%d%Y-%H%S"), average)
+        #newdb.post("/pi1-temp", {"val":average, "time":datetime.now().strftime("%b%d%Y-%H:%M")})
+        newdb.post("/pi1-temp", {"val":average, "time":time.mktime(datetime.now().timetuple())})
+        print('uplode to database')
+        new[:]=[]
+        #os.remove("save.txt")
+        open('save.txt', 'w').close()
+        print("save.txt has been clear")
     else:
-        schedule.run_pending()
-        high=newdb.get('pi1-pond1', "high")
-        low=newdb.get('pi1-pond1', "low")
-        if b != a:
-            newdb.put('pi1-pond1',"temp",a)
-            b=a
-        c=newdb.get('pi1-pond1', "au to")
-        if c == 0:
-            print("Automatic: off");
-            d = newdb.get('pi1-pond1', "ch1")
-            e = newdb.get('pi1-pond1', "ch2")
-            if d == 1:
-                GPIO.output(Relay_Ch1,GPIO.LOW)
-                print("Channel 1:ON")
-            else:
-                GPIO.output(Relay_Ch1,GPIO.HIGH)
-                print("Channel 1:OFF")
-            if e == 1:
-                GPIO.output(Relay_Ch2,GPIO.LOW)
-                print("Channel 2:ON")
-            else:
-                GPIO.output(Relay_Ch2,GPIO.HIGH)
-                print("Channel 2:OFF")
-        else:
-            if a > high or a < low:
-                GPIO.output(Relay_Ch1,GPIO.LOW)
-                print("Channel 1:ON")
-                newdb.put('pi1-pond1',"ch1",1)
-            else:
-                GPIO.output(Relay_Ch1,GPIO.HIGH)
-                print("Channel 1:OFF")
-                newdb.put('pi1-pond1',"ch1",0)
-        time.sleep(1)
+        a=temp.getTemp()
+        print(a)
+        new.append(a)
+        b=str(a)
+        with open("save.txt", "a") as f:
+            f.writelines([b, '\n'])
+        time.sleep(5)
+"""
+
+def up():
+    nsum=0
+    nsum+=sum(new)
+    average=nsum/len(new)
+    newdb.post("/pi1-temp-fore", {"val":average, "time":time.mktime(datetime.now().timetuple())})
+    print('uplode to database')
+    new[:]=[]
+    open('save.txt', 'w').close()
+    print("save.txt has been clear")
+
+
+def upa():
+    nsum1=0
+    nsum1+=sum(new1)
+    average1=nsum1/len(new1)
+    newdb.post("/pi1-temp", {"val":average1, "time":time.mktime(datetime.now().timetuple())})
+    print('uplode to database')
+    new1[:]=[]
+    open('save1.txt', 'w').close()
+    print("save1.txt has been clear")
+
+def rec():
+    a=temp.getTemp()
+    print(a)
+    new.append(a)
+    new1.append(a)
+    b=str(a)
+    with open("save.txt", "a") as f:
+        f.writelines([b, '\n'])
+    with open("save1.txt", "a") as f1:
+        f1.writelines([b, '\n'])
     
+
+
