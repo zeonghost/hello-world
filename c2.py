18ac1ec08b0e92ba8c43a9941188a9b12ab22e86
Tong Tiantai <wyexttt@163.com>
2019-09-08 15:59:56 +0800

    Add files via upload

--- c2.py
+++ c2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import temp
-#import tempuplode
+import tempuplode
 import RPi.GPIO as GPIO
 import time
 import schedule
@@ -25,19 +25,21 @@ GPIO.setup(Relay_Ch1,GPIO.OUT)
 GPIO.setup(Relay_Ch2,GPIO.OUT)
 GPIO.setup(Relay_Ch3,GPIO.OUT)
 
-#GPIO.output(Relay_Ch1,GPIO.HIGH)
-#GPIO.output(Relay_Ch2,GPIO.HIGH)
-#GPIO.output(Relay_Ch3,GPIO.HIGH)
+GPIO.output(Relay_Ch1,GPIO.HIGH)
+GPIO.output(Relay_Ch2,GPIO.HIGH)
+GPIO.output(Relay_Ch3,GPIO.HIGH)
 #relay setup defult off
 
 high=400
 low=200
 
 b=300
+e=0
 
 while True:
     a=temp.getTemp()
-    #tempuplode.rec()
+    print("This is control.py")
+    tempuplode.rec()
     exit_code = os.system('ping -c 1 www.google.com')
     if exit_code:
         print("No Internet Connection, Offline Model")
@@ -59,38 +61,26 @@ while True:
             high=newdb.get('pi1-pond1', "high")
             low=newdb.get('pi1-pond1', "low")
             d = newdb.get('pi1-pond1', "ch1")
-            if a > high or a < low:
-                newdb.put('pi1-push',"warn",1)
             if d == 1:
                 GPIO.output(Relay_Ch1,GPIO.LOW)
                 print("Channel 1:ON")
             else:
                 GPIO.output(Relay_Ch1,GPIO.HIGH)
                 print("Channel 1:OFF")
-            f = newdb.get('pi1-pond1', "ch3")
-            if f == 1:
-                GPIO.output(Relay_Ch3,GPIO.LOW)
-                print("Channel 3:ON")
-            else:
-                GPIO.output(Relay_Ch3,GPIO.HIGH)
-                print("Channel 3:OFF")
-            g = newdb.get('pi1-pond1', "ch2")
-            if g == 1:
-                GPIO.output(Relay_Ch2,GPIO.LOW)
-                print("Channel 2:ON")
-            else:
-                GPIO.output(Relay_Ch2,GPIO.HIGH)
-                print("Channel 2:OFF")
         else:
             if a > high or a < low:
                 GPIO.output(Relay_Ch1,GPIO.LOW)
                 print("Channel 1:ON")
-                newdb.put('pi1-push',"warn",1)
-                newdb.put('pi1-pond1',"ch1",1)
+                if e != 1:
+                     newdb.put('pi1-push',"warn",1)
+                     newdb.put('pi1-pond1',"ch1",1)
+                e = 1
             else:
                 GPIO.output(Relay_Ch1,GPIO.HIGH)
                 print("Channel 1:OFF")
-                newdb.put('pi1-pond1',"ch1",0)
+                if e != 0:
+                     newdb.put('pi1-pond1',"ch1",0)
+                e = 0
         time.sleep(1)
     
 
--- ch2.py
+++ ch2.py
@@ -18,8 +18,13 @@ GPIO.output(Relay_Ch2,GPIO.HIGH)
 
 newdb = firebase.FirebaseApplication('https://aquaculture-7393d.firebaseio.com/')
 
-gap1 = 600
-gap2 = 1800
+exit_code = os.system('ping -c 1 www.google.com')
+if exit_code:
+    gap1 = 60
+    gap2 = 1800
+else:
+    gap1 = newdb.get('pi1-pond1', 'gap1')
+    gap2 = newdb.get('pi1-pond1', 'gap2')
 
 while True:
     exit_code = os.system('ping -c 1 www.google.com')
@@ -33,6 +38,12 @@ while True:
     else:
         c=newdb.get('pi1-pond1', "auto")
         if c == 1:
+            gap1 = newdb.get('pi1-pond1', 'gap1')
+            gap2 = newdb.get('pi1-pond1', 'gap2')
+            print('Gap1: ')
+            print(gap1)
+            print('Gap2: ')
+            print(gap2)
             GPIO.output(Relay_Ch2,GPIO.LOW)
             print("Channel 2:ON")
             newdb.put('pi1-pond1',"ch2",1)
@@ -49,9 +60,11 @@ while True:
             print('Gap2: ')
             print(gap2)
             GPIO.output(Relay_Ch2,GPIO.LOW)
+            newdb.put('pi1-pond1',"ch2",1)
             print("Channel 2:ON")
             time.sleep(gap1)
             GPIO.output(Relay_Ch2,GPIO.HIGH)
+            newdb.put('pi1-pond1',"ch2",0)
             print("Channe2 2:OFF")
             time.sleep(gap2)
         
